60fff72e5b32ae4bfda7aa2966797145e92af546
Kiwi <agiannoulis@mavestro.co>
2021-09-03 04:09:56 +0300

    prints data

--- module/server.go
+++ module/server.go
@@ -2,6 +2,7 @@ package module
 
 import (
 	"context"
+	"fmt"
 	"github.com/ProjectAthenaa/sonic-core/protos/module"
 	"github.com/ProjectAthenaa/sonic-core/sonic"
 	"github.com/ProjectAthenaa/sonic-core/sonic/antibots/shape"
@@ -33,6 +34,8 @@ func init() {
 }
 
 func (s Server) Task(_ context.Context, data *module.Data) (*module.StartResponse, error) {
+	v, _ := json.Marshal(data)
+	fmt.Println(string(v))
 	task := NewTask(data)
 	if err := task.Start(data); err != nil {
 		return nil, err
