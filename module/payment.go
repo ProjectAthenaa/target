35ca9ed579b386aecbe1b91ffa51f304056154d9
Kiwi <agiannoulis@mavestro.co>
2021-09-19 14:50:54 +0300

    update deps

--- module/payment.go
+++ module/payment.go
@@ -3,6 +3,7 @@ package module
 import (
 	"fmt"
 	"github.com/ProjectAthenaa/sonic-core/protos/module"
+	"github.com/prometheus/common/log"
 	"strconv"
 	"strings"
 )
@@ -106,7 +107,8 @@ func (tk *Task) CompareCard() {
 	if strings.Contains(string(res.Body), "SUCCESS") {
 		tk.SetStatus(module.STATUS_CHECKING_OUT, "card valid")
 	} else {
-		tk.SetStatus(module.STATUS_CHECKOUT_DECLINE, "card not valid")
+		log.Info(string(res.Body))
+		tk.SetStatus(module.STATUS_CHECKOUT_ERROR, "card not valid")
 		tk.Stop()
 		return
 	}
